9eb7f0574f0dd986319b0c912c547853e40b4338
DidacVG <didac.valles@enti.cat>
2025-09-23 19:38:50 +0200

    bala acabada

--- Assets/Scripts/Bala.cs
+++ Assets/Scripts/Bala.cs
@@ -5,17 +5,25 @@ using UnityEngine.InputSystem;
 public class Bala : MonoBehaviour
 {
 
-    Rigidbody rb;
+    public float bulletSpeed = 10;
+    public Rigidbody bullet;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
+    void Fire()
+    {
+        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
+        bulletClone.linearVelocity = transform.forward * bulletSpeed;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (UnityEngine.Input.GetButtonDown("Fire1"))
+            Fire();
     }
 
     void Gravedad()
